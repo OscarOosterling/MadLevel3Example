c9349f87d65cd59303b4fcbc01282719bff3002a
Oscar Oosterling <oscar.oosterling@hva.nl>
2020-09-18 21:32:59 +0200

    completed step 4

--- app/src/main/java/com/example/madlevel3example/RemindersFragment.kt
+++ app/src/main/java/com/example/madlevel3example/RemindersFragment.kt
@@ -1,29 +1,45 @@
 package com.example.madlevel3example
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.fragment.findNavController
+import androidx.fragment.app.setFragmentResultListener
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.madlevel2example.Reminder
+import com.example.madlevel2example.ReminderAdapter
+import kotlinx.android.synthetic.main.fragment_reminders.*
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class RemindersFragment : Fragment() {
 
+    private val reminders= arrayListOf<Reminder>()
+    private val reminderAdapter = ReminderAdapter(reminders)
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reminder, container, false)
+        return inflater.inflate(R.layout.fragment_reminders, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        initViews()
 
     }
+
+    private fun initViews() {
+        rvReminders.layoutManager = LinearLayoutManager(context,RecyclerView.VERTICAL,false)
+        rvReminders.adapter = reminderAdapter
+        rvReminders.addItemDecoration(DividerItemDecoration(context,DividerItemDecoration.VERTICAL))
+    }
+
+
 }
